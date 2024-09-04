054d169c4f4fc834b39475c780899927fb0a1a0c
Qishen Zhang <qishen23@gmail.com>
2024-09-04 09:35:15 -0400

    Test GPT on CVE-2017-12378 for bug fixin in 3D specs

--- GenParserDSL.py
+++ GenParserDSL.py
@@ -56,34 +56,28 @@ def process_large_text(text, chunk_size=2000):
     response = call_openai_api(system_contents=chunks)
     return response
 
+def extract_dsl_into_3d(text_3d_lang_specs, text_untar_latest):
+    extract_dsl_question = "Understand the 3D language specification and extract the C code into \
+        The 3d Dependent Data Description language"
 
-if __name__ == "__main__":
-    text_3d_lang_specs = read_file_into_text("./data/3d-lang.rst")
-    text_untar_latest = read_file_into_text("./data/untar.c")
+    # result = process_large_text("\n".join(texts), chunk_size=10000)
+    result = call_openai_api(
+        # System role
+        ["Read and learn the following 3D language documentation: " + text_3d_lang_specs, 
+         "Read and understand the following code in C language: " + text_untar_latest], 
+        # Assistant role
+        [], 
+        # User role
+        [extract_dsl_question])
+    return result
+    
+
+def fix_cve_2009_1270(text_3d_lang_specs, text_untar_latest):
     text_untar_negsize = read_file_into_text("./data/untar_negsize.c")
     text_untar_negsize_fixed = read_file_into_text("./data/untar_negsize_fixed.c")
 
-    extract_dsl_question = "Understand the 3D language specification and extract the C code into \
-        The 3d Dependent Data Description language"
     extract_dsl_question2 = "Understand the 3D language specification and extract the C code into \
         The 3d Dependent Data Description language for both untar_negsize.c and untar_negsize_fixed.c"
-    
-    texts = [
-        text_3d_lang_specs, 
-        text_untar_latest, 
-        extract_dsl_question
-    ]
-
-    # result = process_large_text("\n".join(texts), chunk_size=10000)
-
-    # result = call_openai_api(
-    #     # System role
-    #     ["Read and learn the following 3D language documentation: " + text_3d_lang_specs, 
-    #      "Read and understand the following code in C language: " + text_untar_latest], 
-    #     # Assistant role
-    #     [], 
-    #     # User role
-    #     [extract_dsl_question])
     
     result = call_openai_api(
         ["Read and learn the following 3D language documentation: " + text_3d_lang_specs, 
@@ -95,8 +89,41 @@ if __name__ == "__main__":
             extract_dsl_question2
         ]
     )
+    return result
 
-    print(result)
+def fix_cve_2017_12378(text_3d_lang_specs, text_untar_latest):
+    text_untar_bb11946 = read_file_into_text("./data/untar_bb11946.c")
+    text_untar_bb11946_fixed = read_file_into_text("./data/untar_bb11946_fixed.c")
+
+    # There are several CVEs fixed in ClamAV 0.99.3
+    # https://blog.clamav.net/2018/01/clamav-0993-has-been-released.html
+    
+    # extract_dsl_question3 = "Understand the 3D language specification and extract the C code into \
+    #     The 3d Dependent Data Description language for both untar_bb11946.c and untar_bb11946_fixed.c"
+    extract_dsl_question3 = "Understand the 3D language specification, \
+        find the difference between untar_bb11946.c and untar_bb11946_fixed.c, \
+        and extract the C code into The 3d Dependent Data Description language that fixes the bug."
+    
+    result = call_openai_api(
+        ["Read and learn the following 3D language documentation: " + text_3d_lang_specs, 
+         "Read and understand the following code in untar_bb11946.c: " + text_untar_bb11946, 
+         "Read and understand the following code in untar_bb11946_fixed.c: " + text_untar_bb11946_fixed],
+        [],
+        [
+            extract_dsl_question3
+        ]
+    )
+    return result
+
+if __name__ == "__main__":
+    text_3d_lang_specs = read_file_into_text("./data/3d-lang.rst")
+    text_untar_latest = read_file_into_text("./data/untar.c")
+
+    # result = extract_dsl_into_3d(text_3d_lang_specs, text_untar_latest)
+    # result = fix_cve_2009_1270(text_3d_lang_specs, text_untar_latest)
+    result = fix_cve_2017_12378(text_3d_lang_specs, text_untar_latest)
+
+    print(result) 
 
     # Write the text to the file
     if result is not None:
